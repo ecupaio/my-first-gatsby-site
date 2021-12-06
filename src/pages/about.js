55e424c2f4efac6d5eb3ce65fa3c4b438d374d39
Ed Cupaioli <ecupaio@gmail.com>
2021-12-06 10:50:08 -0800

    added home and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! i'm the proud creator od this site which I built with Gatsby</p>
+    </main>
+  )
+}
+
+export default AboutPage

5fdeaa584ca7313ff31650bb3abe916ef2d506cf
Ed Cupaioli <ecupaio@gmail.com>
2021-12-06 11:40:34 -0800

    added css to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,21 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { 
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return(
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>Home</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
@@ -16,7 +23,7 @@ const Layout = ({ pageTitle, children }) => {
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
